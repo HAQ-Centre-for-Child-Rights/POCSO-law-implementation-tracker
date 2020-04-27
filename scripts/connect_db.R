fa61894666ad1adabf7433ca1bd041002cdf972d
apoorv <apoorv7491@gmail.com>
2020-04-27 12:41:17 +0530

    add scripts

---
+++ scripts/connect_db.R
@@ -0,0 +1,17 @@
+source("scripts/libraries.R")
+
+db <- Sys.getenv("db_name")
+host_db <- Sys.getenv("host_db")
+db_port <- Sys.getenv("db_port")
+db_user <- Sys.getenv("postgres_user")
+db_password <- Sys.getenv("postgres_pwd")
+
+con <-
+  dbConnect(
+    RPostgres::Postgres(),
+    dbname = db,
+    host = host_db,
+    port = db_port,
+    user = db_user,
+    password = db_password
+  )
