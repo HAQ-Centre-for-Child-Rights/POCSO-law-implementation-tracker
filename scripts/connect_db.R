26c274e25e1b71a9510c0a6081d9b0b649f12553
apoorv <apoorv7491@gmail.com>
2020-04-27 13:09:44 +0530

    lis t all tables

--- scripts/connect_db.R
+++ scripts/connect_db.R
@@ -7,11 +7,18 @@ db_user <- Sys.getenv("postgres_user")
 db_password <- Sys.getenv("postgres_pwd")
 
 con <-
-  dbConnect(
-    RPostgres::Postgres(),
+  dbxConnect(adapter = "postgres",
     dbname = db,
     host = host_db,
     port = db_port,
     user = db_user,
     password = db_password
   )
+
+# Sample Queries
+
+# 1 - List all tables ('https://stackoverflow.com/questions/43720911/list-tables-within-a-postgres-schema-using-r')
+# case_details <-
+#   dbxSelect(con,
+#             "SELECT table_name FROM information_schema.tables
+#                    WHERE table_schema='public'")
