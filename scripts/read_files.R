feef8c8b4c9933b5df636d8d2cfda74bed935ce2
apoorv <apoorv7491@gmail.com>
2020-04-29 21:39:08 +0530

    Add read-files

---
+++ scripts/read_files.R
@@ -0,0 +1,13 @@
+# 1 - Case Details
+case_details <- read_csv("data/ipc_pocso_patterns/case_details.csv", 
+                         col_types = cols(date_of_decision = col_date(format = "%Y-%m-%d"), 
+                                          district_code = col_character(), 
+                                          dt_regis = col_date(format = "%Y-%m-%d"), 
+                                          state_code = col_character()))
+# 2 - Act Details
+act_details <- read_csv("data/ipc_pocso_patterns/act_details.csv", 
+                        col_types = cols(id = col_character()))
+
+# 3 - Case Act Details
+case_act_details <- read_csv("data/ipc_pocso_patterns/case_act_details.csv", 
+                             col_types = cols(act = col_character()))
