7a8e6761d9b148a9d861740ae341af56bbfd269d
Apoorv Anand <apoorv7491@gmail.com>
2020-07-13 15:20:44 +0530

    add logic for local/remote con

--- scripts/connect_db.R
+++ scripts/connect_db.R
@@ -1,8 +1,17 @@
-db <- Sys.getenv("db_name")
-host_db <- Sys.getenv("host_db")
-db_port <- Sys.getenv("db_port")
-db_user <- Sys.getenv("postgres_user")
-db_password <- Sys.getenv("postgres_pwd")
+# Connect to local/remote DB
+server_flag <- 'l' # l (local) / r (remote)
+if(server_flag == 'l'){
+  con_string <- 'local'
+} else if (server_flag == 'r'){
+  con_string <- ''
+}
+
+db <- Sys.getenv(glue("{con_string}db_name"))
+host_db <- Sys.getenv(glue("{con_string}host_db"))
+db_port <- Sys.getenv(glue("{con_string}db_port"))
+db_user <- Sys.getenv(glue("{con_string}postgres_user"))
+db_password <- Sys.getenv(glue("{con_string}postgres_pwd"))
+
 
 con <-
   dbxConnect(adapter = "postgres",
