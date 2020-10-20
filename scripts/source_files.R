bde94aa294bb0855378d2c0c89860801b5f30ab8
Apoorv Anand <apoorv7491@gmail.com>
2020-10-20 10:10:31 +0530

    Use 'here' for file paths.

--- scripts/source_files.R
+++ scripts/source_files.R
@@ -1,6 +1,6 @@
 # The paths are with respect to the Rmd files (under research)
 
-source("../scripts/libraries.R")
-source("../scripts/connect_db.R")
-source("../scripts/functions.R")
-source("../scripts/patterns_for_case_ids.R")
+source(here("scripts", "libraries.R"))
+source(here("scripts", "connect_db.R"))
+source(here("scripts","functions.R"))
+source(here("scripts","patterns_for_case_ids.R"))
